2e0d81e3b92e77e336d4340525c4ca10c3c0127d
Pierre Vassoilles <pvassoilles@intuitiv.fr>
2017-01-25 15:36:15 +0100

    Add two new types : Email & URL In twig, add jQuery if not included

---
+++ IT/InputMaskBundle/Form/Type/EmailMaskType.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pvassoilles
+ * Date: 16/01/17
+ * Time: 11:28
+ */
+
+namespace IT\InputMaskBundle\Form\Type;
+
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmailMaskType extends EmailType
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+
+        $view->vars['mask_alias'] = $options['mask-alias'];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefault('mask-alias', 'email');
+        $resolver->setDefault('attr', array(
+            'class' => 'form-control',
+        ));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return 'it_email_mask';
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function getBlockPrefix()
+    {
+        return 'it_alias_mask';
+    }
+
+}
---
+++ IT/InputMaskBundle/Form/Type/UrlMaskType.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pvassoilles
+ * Date: 16/01/17
+ * Time: 11:28
+ */
+
+namespace IT\InputMaskBundle\Form\Type;
+
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UrlMaskType extends TextType
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+
+        $view->vars['mask_alias'] = $options['mask-alias'];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefault('mask-alias', 'url');
+        $resolver->setDefault('attr', array(
+            'class' => 'form-control',
+        ));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return 'it_url_mask';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getBlockPrefix()
+    {
+        return 'it_alias_mask';
+    }
+
+}
